a5f4051d13e8e3f5fd0946adc1355ebf7893d754
listenrightmeow <mike@fatt.ist>
2017-10-02 21:44:04 -0700

    reset scss file

--- grunt/sass.js
+++ grunt/sass.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   dist: {
     files: {
+      'application/dist/stylesheets/reset.css' : 'application/stylesheets/reset.scss'
       'application/dist/stylesheets/index.css' : 'application/stylesheets/index.scss'
     }
   }
